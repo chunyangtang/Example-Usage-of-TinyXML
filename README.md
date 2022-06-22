260343a650e37074eb465838117a5f0224ee7f2a
Tang Chunyang <tangcy03@outlook.com>
2022-06-22 13:35:40 +0800

    Mainly finish read part of test.cpp, refine readme, add xmltest.cpp and .gitignore

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Tcy's 2022 OOP Assignment
 <b>The repo is not for teaching purpose, and no advance usages of TinyXML-2 are expected to be found in this repo.</b>
 
 This project is created in a Linux environment (Ubuntu 20.04 LTS over wsl). Results should be reproducible by simply compiling and running code on a similar environment through VS Code or use command "make main" to compile and "./main" to run in Linux shell.
+
+Among all source code files, tinyxml2.cpp tinyxml2.h xmltest.cpp are code from tinyxml2 project, other source code files are created by tcy in reference to them.
--- test.cpp
+++ test.cpp
@@ -1,10 +1,72 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include "tinyxml2.h"
 
+// Classes, varibles and functions are declared in the namespace of tinyxml2
 using namespace tinyxml2;
 
 int main()
 {
+    // Create an object to contain the overall xml document
+    XMLDocument doc;
+    /**
+        Load XML File from disk.
+        Can also use Parse(const char* xml, size_t nBytes=static_cast<size_t>(-1)) to load xml file from string.
+        @verbatim
+        static const char* xml =
+            "<?xml version=\"1.0\"?>"
+            "<!DOCTYPE PLAY SYSTEM \"play.dtd\">"
+            "<PLAY>"
+            "<TITLE>A Midsummer Night's Dream</TITLE>"
+            "</PLAY>";
+        doc.Parse( xml );
+        @endverbatim
+        or use LoadFile( FILE* ) to load file from a FILE pointer
+    */
+    doc.LoadFile("source.xml");
+    // XMLHandle can safely handle error (so no need to have a bunch of if(){}...) and safe to copy.
+    XMLHandle docHandle(&doc);
+
+    // Output each type of node to demostrate doc have loaded everything
+    // use FirstChildElement() or RootElement() to get the root element of an xml file
+    XMLElement *rootchild = docHandle.FirstChildElement().ToElement();
+    if (rootchild)
+    {
+        // get Element's name
+        std::cout << "   " << rootchild->Name() << "   \n";
+        // Get 2 Attributes' name and value;
+        const char *Attri_name_1 = rootchild->FirstAttribute()->Name();
+        std::cout << "   " << Attri_name_1 << " : " << rootchild->Attribute(Attri_name_1) << std::endl;
+        const char *Attri_name_2 = rootchild->FirstAttribute()->Next()->Name();
+        std::cout << "   " << Attri_name_2 << " : " << rootchild->Attribute(Attri_name_2) << std::endl;
+    }
+    std::cout << "-------------------------\n";
+    // get child element from root
+    XMLElement *foodchild = docHandle.FirstChildElement().FirstChildElement("food").ToElement();
+    for (int i = 1;; i++)
+    {
+        if (foodchild) // To avoid invalid memory access
+        {
+            std::cout << "Food " << i << "   ";
+            // get attribute names
+            const char *food_attri = foodchild->FirstAttribute()->Name();
+            // IntAttribute return integer form of attribute (Notice that all attributes' value should be given in a string form)
+            std::cout << food_attri << " : " << foodchild->IntAttribute(food_attri) << std::endl;
+            XMLElement *childelement;
+            // output child elements' tag and content of food
+            for (childelement = foodchild->FirstChildElement(); childelement != foodchild->LastChildElement(); childelement = childelement->NextSiblingElement())
+            {
+                std::cout << std::setw(12) << std::right << childelement->Name() << " : " << childelement->GetText() << std::endl;
+            }
+            std::cout << std::setw(12) << std::right << childelement->Name() << " : " << childelement->GetText() << std::endl;
+        }
+        else
+        {
+            break;
+        }
+        foodchild = foodchild->NextSiblingElement();
+        std::cout << "-------------------------\n";
+    }
     return 0;
 }
